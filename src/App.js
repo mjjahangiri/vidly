01d47a1ad7c5e1a30eb80ce873137504565af391
MJJ <mj.jahangiri.6@gmail.com>
2022-02-23 01:00:14 +0330

    add componentDidMount for State

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Movies from './Pages/Movies';
-import Favorite from './Pages/Favorite';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Movie from './Pages/Movie';
-import NotFound from './Pages/NotFound';
-import Header from './Components/Header';
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Movies from "./Pages/Movies";
+import Favorite from "./Pages/Favorite";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Movie from "./Pages/Movie";
+import NotFound from "./Pages/NotFound";
+import Header from "./Components/Header";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/movies" exact element={<Movies />} />
-        <Route path="/favorite" exact element={<Favorite />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/movie/:id" exact element={<Movie />} />
-        <Route path="*" exact element={<NotFound />} />
-      </Routes>
-    </>
-  );
+export default class App extends Component {
+  state = {
+    movies: [],
+    genres: [],
+  };
+  async componentDidMount() {
+    const { data: movies } = await axios.get("http://localhost:3001/Movies");
+    const { data: genres } = await axios.get("http://localhost:3001/Genres");
+    this.setState({ movies, genres });
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/movies" exact element={<Movies />} />
+          <Route path="/favorite" exact element={<Favorite />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/register" exact element={<Register />} />
+          <Route path="/movie/:id" exact element={<Movie />} />
+          <Route path="*" exact element={<NotFound />} />
+        </Routes>
+      </>
+    );
+  }
 }
-
-export default App;
